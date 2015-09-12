68bfafd4fd03830047e26dc17f1df475569cca83
midspace <midspace@hotmail.com>
2015-09-12 21:25:14 +1000

    Pay issues #19
    Fixed issue where player can still send negative values.
    Cleaned up pay logic.
    As the Account classes are static they are by Reference. They can
    updated directly.
    Online checks are not required, as SE will handle that part of the
    message.
    The rest of the pseudo code probably isn't required any more.

--- Economy/Data/Scripts/Economy.scripts/Messages/MessagePayUser.cs
+++ Economy/Data/Scripts/Economy.scripts/Messages/MessagePayUser.cs
@@ -1,10 +1,8 @@
 ﻿namespace Economy.scripts.Messages
 {
     using System;
-    using Config;
     using ProtoBuf;
     using Sandbox.ModAPI;
-    using System.Linq;
     using Economy.scripts;
 
     [ProtoContract]
@@ -27,7 +25,7 @@
         public override void ProcessServer()
         {
             // flag used when transferring funds eg /pay bob 10 - indicates if bob even has an account record yet
-            bool hasAccount = false;
+            //bool hasAccount = false;
 
             //string alias; //represents players current in game nickname
             //string timestamp; //will be used for seen command later maybe
@@ -42,15 +40,17 @@
 
             var accountToSpend = EconomyScript.Instance.BankConfigData.FindOrCreateAccount(SenderSteamId, SenderDisplayName);
 
-            //* It needs to first check the player has enough to cover his payment
-            var bankBalance = Convert.ToDecimal(accountToSpend.BankBalance);
-            if (TransactionAmount <= bankBalance || payingPlayer.IsAdmin())
+            // need fix negative amounts before checking if the player can afford it.
+            if (!payingPlayer.IsAdmin())
+                TransactionAmount = Math.Abs(TransactionAmount);
+
+            // It needs to first check the player has enough to cover his payment
+            if (TransactionAmount <= accountToSpend.BankBalance || payingPlayer.IsAdmin())
             // do we have enough or are we admin so it doesnt matter
             //*      if true, 
             {
-                //*          it needs to check the person being paid has an account record, 
-                var account = EconomyScript.Instance.BankConfigData.Accounts.FirstOrDefault(
-                    a => a.NickName.Equals(ToUserName, StringComparison.InvariantCultureIgnoreCase));
+                // it needs to check the person being paid has an account record, 
+                var account = EconomyScript.Instance.BankConfigData.FindAccount(ToUserName);
 
                 //*               if false, 
                 //*                  it needs to check if the other player is even online
@@ -76,53 +76,31 @@
 
                 //*          if hasaccount bool true   
 
-                //is there a modify property to save the need to remove then re-add? 
-                //here we remove this players bank record
-                EconomyScript.Instance.BankConfigData.Accounts.Remove(accountToSpend);
-                if (!payingPlayer.IsAdmin())
-                {
-                    TransactionAmount = Math.Abs(TransactionAmount);
-                }
-                //admins can give or take money, normal players can only give money so convert negative to positive
-                //here we add the players bank record again with the updated balance minus what they spent
-                accountToSpend = new BankAccountStruct()
-                {
-                    BankBalance = (bankBalance - TransactionAmount),
-                    Date = DateTime.Now,
-                    NickName = SenderDisplayName,
-                    SteamId = SenderSteamId
-                };
-                EconomyScript.Instance.BankConfigData.Accounts.Add(accountToSpend);
+                // is there a modify property to save the need to remove then re-add? 
+                // admins can give or take money, normal players can only give money so convert negative to positive
+                // here we add the players bank record again with the updated balance minus what they spent
+                accountToSpend.BankBalance -= TransactionAmount;
+                accountToSpend.Date = DateTime.Now;
 
-                //here we retrive the target player steam id and balance
-                ulong theirID = account.SteamId;
-                decimal theirbank = (account.BankBalance += TransactionAmount);
-                string theirnick = account.NickName;
-                //here we clean out the old data
-                EconomyScript.Instance.BankConfigData.Accounts.Remove(account);
-                //here we build a new record with the correct data
-                account = new BankAccountStruct()
-                {
-                    BankBalance = theirbank,
-                    Date = DateTime.Now,
-                    NickName = theirnick,
-                    SteamId = theirID
-                };
-                //here we write it back to our bank ledger file
-                EconomyScript.Instance.BankConfigData.Accounts.Add(account);
+                // here we retrive the target player steam id and balance
+                // here we write it back to our bank ledger file
+                account.BankBalance += TransactionAmount;
+                account.Date = DateTime.Now;
 
-                //if this works this is a very sexy way to work with our file
-                //testing: it does indeed work, if i was a teenager id probably need to change my underwear at this point
+                // if this works this is a very sexy way to work with our file
+                // testing: it does indeed work, if i was a teenager id probably need to change my underwear at this point
 
-                // now need to work out how to notify receiving player that they were paid and/or any message the sending player wrote
+                // This notifies receiving player that they were paid and/or any message the sending player wrote
                 // which needs to not send if the player isnt online - pity ive no idea how to write to the faction chat system
                 // be a good place to send the player a faction message as it would work even if they were offline..
-
                 MessageClientTextMessage.SendMessage(account.SteamId, "PAY",
-                    string.Format("{0}, {1} just paid you {2} for {3}", theirnick, SenderDisplayName, TransactionAmount, Reason));
+                    string.Format("{0}, {1} just paid you {2} for {3}", account.NickName, SenderDisplayName, TransactionAmount, Reason));
 
                 MessageClientTextMessage.SendMessage(SenderSteamId, "PAY",
-                    string.Format("You just paid {0}, {2} for {3}", theirnick, SenderDisplayName, TransactionAmount, Reason));
+                    string.Format("You just paid {0}, {1} for {2}", account.NickName, TransactionAmount, Reason));
+
+                EconomyScript.Instance.ServerLogger.Write("Pay: '{0}' sent {1} to '{2}'", accountToSpend.NickName, TransactionAmount, ToUserName);
+
 
                 //*      if false/otherwise throw error you dont have enough money
             }
