8fba5f2dc657166180c0ce84bd9d26a92545011f
midspace <midspace@hotmail.com>
2015-10-12 19:53:56 +1100

    #47
    Further updated trade state details.

--- Economy/Data/Scripts/Economy.scripts/EconConfig/TradeState.cs
+++ Economy/Data/Scripts/Economy.scripts/EconConfig/TradeState.cs
@@ -16,7 +16,12 @@
         /// </summary>
         Buy,
 
-        // TODO: other Buy states.
+        /// <summary>
+        /// market buy offer - goes to 0,0,0 stock exchange or a registered market 
+        /// (eg faction/merchant/station etc)- its an open ended offer available to anyone 
+        /// to fill within range of the desired market territory
+        /// </summary>
+        BuyWanted,
 
         /// <summary>
         /// Trader is selling something.
@@ -24,9 +29,16 @@
         Sell,
 
         /// <summary>
-        /// Trade directly to a player
+        /// We are selling/offering directly to a particular player.
         /// </summary>
         SellDirectPlayer,
+
+        /// <summary>
+        /// market sell offer - goes to 0,0,0 stock exchange or a registered market 
+        /// (eg faction/merchant/station etc)- its an open ended offer available to anyone 
+        /// to buy within range of the desired market territory
+        /// </summary>
+        SellOffer,
 
         /// <summary>
         /// The Sell was rejected, and the items held for return.
@@ -38,10 +50,16 @@
         /// </summary>
         SellTimedout,
 
-        // whatever these are, their purpose needs to be described exactly.
-        //Frozen, ?
-        //Holding, ?
-        //Offer, ?
-        //ToPlayer, ?
+        /// <summary>
+        /// transaction/funds have been frozen - either the admin has suspended trading or the 
+        /// server is shutting down and/or both the sender and receiver is offline - basically timer frozen
+        /// </summary>
+        Frozen,
+
+        /// <summary>
+        /// funds/items being held due to buyer/seller or payer or payee being offline; but the 
+        /// timer has expired.
+        /// </summary>
+        Holding,
     }
 }
