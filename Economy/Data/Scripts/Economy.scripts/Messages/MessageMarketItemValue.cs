4423b4e58a58dd3a080b73912d6da7b93db1a9b7
xPhoenixXx <jpcsupplies@hotmail.com>
2016-10-19 00:02:54 +1100

    Manual Merge

--- Economy/Data/Scripts/Economy.scripts/Messages/MessageMarketItemValue.cs
+++ Economy/Data/Scripts/Economy.scripts/Messages/MessageMarketItemValue.cs
@@ -79,13 +79,17 @@
             {
                 // TODO: qty may need additional range checking here.
 
-                if (Quantity == 1)
+                if (Quantity == 1) {
                     // set reply to report back the current buy and sell price only since that is all we asked for
-                    reply = String.Format("TRADE - You can buy each '{0}' for {1}, or sell it back for {2} each.", DisplayName, item.SellPrice, item.BuyPrice);
-                else
+                    reply = String.Format("Player TRADE - You can buy each '{0}' for {1}, or sell it back for {2} each.", DisplayName, item.SellPrice, item.BuyPrice);
+                    reply += "\r\n" + String.Format("NPC TRADE - You can buy each '{0}' for {1}, or sell it back for {2} each.", DisplayName, ReactivePricing.PriceAdjust(item.SellPrice, item.Quantity), ReactivePricing.PriceAdjust(item.BuyPrice, item.Quantity));
+                }
+                else {
                     // value BLAH 12 - we must want to know how much we make/pay for buying/selling 12
                     // set reply to current buy and sell price multiplied by the requested qty.
-                    reply = String.Format("TRADE - You can buy {0} '{1}' for {2} or sell it back for {3} each.", Quantity, DisplayName, item.SellPrice * Quantity, item.BuyPrice * Quantity);
+                    reply = String.Format("Player TRADE - You can buy {0} '{1}' for {2} or sell it back for {3} each.", Quantity, DisplayName, item.SellPrice * Quantity, item.BuyPrice * Quantity);
+                    reply += "\r\n" + String.Format("NPC TRADE - You can buy {0} '{1}' for {2} or sell it back for {3} each.", Quantity, DisplayName, ReactivePricing.PriceAdjust(item.SellPrice, item.Quantity) * Quantity, ReactivePricing.PriceAdjust(item.BuyPrice, item.Quantity) * Quantity);
+                }
             }
             MessageClientTextMessage.SendMessage(SenderSteamId, "VALUE", reply);
         }
