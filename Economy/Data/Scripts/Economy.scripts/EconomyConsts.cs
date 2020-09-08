065417c2fa2b7b0799aaa441eba9e7dd393fa867
xPhoenixXx <jpcsupplies@hotmail.com>
2020-09-08 10:34:12 +1000

    Update EconomyConsts.cs

--- Economy/Data/Scripts/Economy.scripts/EconomyConsts.cs
+++ Economy/Data/Scripts/Economy.scripts/EconomyConsts.cs
@@ -26,7 +26,7 @@
 
 
         //milestone level A=Alpha B=Beta, dev = development test version or Milestone eg 1.0A Milestone, 1.1A Dev etc
-        public const string MajorVer = "Econ 3.399A +SC";
+        public const string MajorVer = "Econ 4A +refactor+roaming npc zones";
 
         //Name our money
         public const string CurrencyName = "Credits";
